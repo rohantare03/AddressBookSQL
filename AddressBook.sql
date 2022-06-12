83fe0f0c6798e30525435fe8cee90c6013577d81
rohantare03 <rohatare123@gmail.com>
2022-06-12 11:42:00 +0530

    [Rohan] Add : Creating a Table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,15 @@
 create database AddressBook_Service
 use AddressBook_Service
+
+create table AddressBook
+(
+	ID int identity(1,1) primary key,
+	FirstName varchar(20) NOT NULL,
+	LastName varchar(20) NOT NULL,
+	Address varchar(100) NOT NULL,
+	City varchar(20) NOT NULL,
+	State varchar(20) NOT NULL,
+	Zip bigint NOT NULL,
+	PhoneNumber bigint NOT NULL,
+	Email varchar(50) NOT NULL
+);
