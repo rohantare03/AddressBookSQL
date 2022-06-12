0a66a896e75d62ec3dc174d79844fb2338a561fa
rohantare03 <rohatare123@gmail.com>
2022-06-12 11:49:23 +0530

    [Rohan] Add : Inserting data in the table

--- AddressBook.sql
+++ AddressBook.sql
@@ -13,3 +13,9 @@ create table AddressBook
 	PhoneNumber bigint NOT NULL,
 	Email varchar(50) NOT NULL
 );
+
+Insert into AddressBook values('Rohan','Tare','Virar','Mumbai','Maharashtra',401305,9158499217,'rohan@gmail.com')
+Insert into AddressBook values('Hades','Dark','Beverlyhills','Sanfransico','Newyork',100023,9564213370,'hades@gmail.com')
+Insert into AddressBook values('Rahul','Tare','Palhar','Surat','Gujarat',400022,9975062727,'rahul@gmail.com')
+Insert into AddressBook values('Zeus','Light','Mulund','Navimumbai','MH',400031,9956442187,'zeus@gmial.com')
+select * from AddressBook
